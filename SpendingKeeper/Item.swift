81b569ad96277242db185466f798df3a9b8dfc62
Jae-Seung Lee <jaeseung@gmail.com>
2024-03-26 16:59:36 -0400

    Create SpendingKeeper

---
+++ SpendingKeeper/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SpendingKeeper
+//
+//  Created by Jae Seung Lee on 3/26/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
