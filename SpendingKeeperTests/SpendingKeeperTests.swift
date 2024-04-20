206c7b555923b53c65d7f540bfa1152803de410b
Jae-Seung Lee <jaeseung@gmail.com>
2024-04-20 12:01:32 -0400

    Test: Add unit tests against SKViewModel.latestStatementDate(_:)

--- SpendingKeeperTests/SpendingKeeperTests.swift
+++ SpendingKeeperTests/SpendingKeeperTests.swift
@@ -7,9 +7,20 @@
 
 import XCTest
 @testable import SpendingKeeper
+import SwiftData
 
 final class SpendingKeeperTests: XCTestCase {
 
+    @MainActor
+    let testContainer: ModelContainer = {
+        do {
+            let container = try ModelContainer(for: SKAccount.self, SKRecord.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+            return container
+        } catch {
+            fatalError("Failed to create test container")
+        }
+    }()
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -18,12 +29,32 @@ final class SpendingKeeperTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    @MainActor func testLatestStatementDateOne() throws {
+        let viewModel = SKViewModel(modelContext: testContainer.mainContext)
+        
+        let actualDate = viewModel.latestStatementDate(.one)
+        
+        let daysToSubtract = Calendar.current.component(.day, from: .now)
+        let expectedDate = Calendar.current.startOfDay(for: Calendar.current.date(byAdding: .day, value: 1 - daysToSubtract, to: .now)!)
+        
+        XCTAssertEqual(actualDate, expectedDate)
+    }
+    
+    @MainActor func testLatestStatementDateQuarterly() throws {
+        let viewModel = SKViewModel(modelContext: testContainer.mainContext)
+        
+        let actualDate = viewModel.latestStatementDate(.quarterly)
+        
+        let monthNow = Calendar.current.component(.month, from: .now)
+        let yearNow = Calendar.current.component(.year, from: .now)
+        
+        let quarter = monthNow/3 + 1
+        let startMonthofQuater = (quarter - 1) * 3 + 1
+        let dateComponents = DateComponents(year: yearNow, month: startMonthofQuater, day: 1)
+        
+        let expectedDate = Calendar.current.startOfDay(for: Calendar.current.date(from: dateComponents)!)
+
+        XCTAssertEqual(actualDate, expectedDate)
     }
 
     func testPerformanceExample() throws {
