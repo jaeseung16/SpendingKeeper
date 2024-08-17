34b8962bc4c28c38123e44a62249adfffcf8980d
Jae-Seung Lee <jaeseung@gmail.com>
2024-08-17 13:42:19 -0400

    feat: Generate pdf to share

--- SpendingKeeper/View/SnapshotDetailView.swift
+++ SpendingKeeper/View/SnapshotDetailView.swift
@@ -13,7 +13,6 @@ struct SnapshotDetailView: View {
     
     @State var snapshot: SKSnapshot
     @State private var presentShareSheet = false
-    @State private var presentAlert = false
     
     private var sumOfIncome: Double
     private var sumOfSpending: Double
@@ -80,26 +79,10 @@ struct SnapshotDetailView: View {
             }
             .toolbar {
                 ToolbarItem {
-                    Button {
-                        presentShareSheet = true
-                    } label: {
-                        Image(systemName: "square.and.arrow.up")
-                    }
+                    ShareLink("Share", item: renderPdf())
                 }
             }
-            .sheet(isPresented: $presentShareSheet) {
-                if let url = viewModel.generateCSV(from: Date(), to: Date()) {
-                    ShareActivityView(url: url, applicationActivities: nil, failedToRemoveItem: $presentAlert)
-                }
-            }
-            .alert("Failed to remove files", isPresented: $presentAlert) {
-                Button("Dismiss") {
-                    presentAlert = false
-                }
-            }
-                
         }
-        
     }
     
     private var header: some View {
@@ -148,5 +131,96 @@ struct SnapshotDetailView: View {
                         .font(.callout)
                 }
             }
+    }
+    
+    private func renderPdf() -> URL {
+        let url = URL.documentsDirectory.appending(path: "\(snapshot.title).pdf")
+        
+        var box = CGRect(x: 0.0, y: 0.0, width: 612.0, height: 792.0)
+        // https://pdfkit.org/docs/paper_sizes.html
+        // A4 595.28 x 841.89
+        // LETTER 612.00 X 792.00
+        
+        guard let pdf = CGContext(url as CFURL, mediaBox: &box, nil) else {
+            return url
+        }
+        
+        pdf.beginPDFPage(nil)
+        
+        let renderer = ImageRenderer(content: pdfBody)
+        renderer.render { size, renderer in
+            // size=(467.0, 477.30908203125)
+            
+            // Will place the view in the middle of pdf on x-axis
+            let xTranslation = box.size.width / 2.0 - size.width / 2.0
+            let yTranslation = box.size.height / 2.0 - size.height / 2.0
+            // Spacing between the views on y-axis
+            let spacing: CGFloat = 20
+
+            // TODO: - View starts printing from bottom, need to inverse Y position
+            pdf.translateBy(x: xTranslation, y: yTranslation + spacing)
+
+            // Render the SwiftUI view data onto the page
+            renderer(pdf)
+        }
+        
+        pdf.endPDFPage()
+        
+        pdf.closePDF()
+        
+        return url
+    }
+    
+    private var pdfBody: some View {
+        VStack {
+            header
+            
+            Divider()
+            
+            if let spendings = snapshot.spendings {
+                Section {
+                    HStack {
+                        Chart(spendings, id: \.accoundId) {
+                            barMark($0.accountName, $0.total, $0.total / sumOfSpending)
+                        }
+                        .chartYScale(domain: [0, 1.1 * sumOfSpending])
+                        
+                        Chart(spendings, id: \.accoundId) {
+                            sectorMark($0.accountName, $0.total, $0.total / sumOfSpending)
+                        }
+                    }
+                } header: {
+                    HStack {
+                        Text("SPENDING")
+                            .font(.title3)
+                        Spacer()
+                    }
+                }
+                .padding()
+            }
+            
+            if let incomes = snapshot.incomes {
+                Section {
+                    HStack {
+                        Chart(incomes, id: \.accoundId) {
+                            barMark($0.accountName, $0.total, $0.total / sumOfIncome)
+                        }
+                        .chartYScale(domain: [0, 1.1 * sumOfIncome])
+                        
+                        Chart(incomes, id: \.accoundId) {
+                            sectorMark($0.accountName, $0.total, $0.total / sumOfIncome)
+                        }
+                    }
+                } header: {
+                    HStack {
+                        Text("INCOME")
+                            .font(.title3)
+                        Spacer()
+                    }
+                }
+                .padding()
+            }
+        }
+        .frame(width: 540.0, height: 700.0)
     }
 }
